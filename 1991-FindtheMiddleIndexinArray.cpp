7292545c07d67c91964a3b5620fb534cd93e1fb9
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-05-03 16:03:37 -0600

    Create 1991-FindtheMiddleIndexinArray.cpp

---
+++ 1991-FindtheMiddleIndexinArray.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+int findMiddleIndex(std::vector<int>& nums) {
+
+	int right_sum = std::accumulate(nums.begin(), nums.end(), 0);
+	int left_sum = 0;
+
+	for (int i = 0; i < nums.size(); i++) {
+
+		left_sum += nums[i];
+		right_sum -= nums[i];
+
+		if (left_sum - nums[i] == right_sum) {
+			return i;
+		}
+
+	}
+
+	return -1;
+}
+};
