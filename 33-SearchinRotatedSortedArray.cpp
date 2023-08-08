b68f90b2390815fb5bd6b5364388bd6279a38c2f
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-08-07 19:12:38 -0600

    Update 33-SearchinRotatedSortedArray.cpp

--- 33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray.cpp
@@ -7,56 +7,35 @@ int search(std::vector<int>& nums, int target) {
 	int low = 0;
 	int high = nums.size();
 	int mid = (low + high) / 2;
-	int pivot = 0;
 
-	while (low < high) {
+	while (low < high){
 
-		if (nums[mid] > nums.back()) {
-			low = mid + 1;
-		}
-		else if (nums[mid] < nums.back()) {
-			high = mid;
-			pivot = mid;
-		}else{
-			pivot = (nums.back() > nums.front()) ? 0 : nums.size()-1;
-			break;
-		}
-
+		if (nums[mid] == target) return mid;
+		
+		if (nums[mid] > nums.back()) low = mid + 1;
+		
+		else high = mid;
+		
 		mid = (low + high) / 2;
 	}
-
-	if (nums[pivot] == target) {
-		return pivot;
-	}
-	if (nums.back() == target) {
-		return nums.size() - 1;
-	}
-
-
+	
+	if (nums[low] == target) return low;
+	
 	if (target > nums.back()) {
+		high = low;
 		low = 0;
-		high = pivot;
 	}
-	else {
-		low = pivot;
-		high = nums.size();
-	}
-	mid = (low + high) / 2;
+	else high = nums.size();
+	
 	while (low < high) {
 
-		if (nums[mid] == target) {
-			return mid;
-		}
-
-		if (nums[mid] < target) {
-			low = mid + 1;
-		}
-		else {
-			high = mid;
-		}
-
 		mid = (low + high) / 2;
+		if (nums[mid] == target) return mid;
+		
+		if (nums[mid] < target) low = mid+1;
+		
+		else high = mid;
 	}
-	return (nums[low] == target) ? low : -1;
+	return -1;
 }
 };
