249d2bca441fac135db76df7def3745fbbce2f7c
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-08-13 18:46:10 -0600

    Update 215-KthLargestElementinanArray.cpp

--- 215-KthLargestElementinanArray.cpp
+++ 215-KthLargestElementinanArray.cpp
@@ -4,21 +4,28 @@ class Solution {
 public:
 int findKthLargest(std::vector<int>& nums, int k) {
 
-	std::vector<int> count(20001, 0);
+	int smallest_num = INT_MAX;
+	int largest_num = INT_MIN;
+	std::vector<int> counts;
 
 	for (int i = 0; i < nums.size(); i++) {
-		count[nums[i] + 10000]++;
+		smallest_num = (smallest_num < nums[i]) ? smallest_num : nums[i];
+		largest_num = (largest_num > nums[i]) ? largest_num : nums[i];
 	}
 
-	for (int i = 20000; i >= 0; i--) {
+	counts.resize(std::abs(largest_num)+std::abs(smallest_num) + 1, 0);
 
-		k -= count[i];
-		if (k <= 0) {
-			return i - 10000;
+
+	for (int i = 0; i < nums.size(); i++) {
+		counts[nums[i] + std::abs(smallest_num)]++;
+	}
+
+	for (int i = counts.size() - 1; i >= 0; i--) {
+		if (counts[i]) {
+			k -= counts[i];
+			if (k <= 0) return i - std::abs(smallest_num);
 		}
 	}
-
-
 	return 0;
 }
 };
