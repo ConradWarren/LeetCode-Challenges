fe02ae9cea9e6e9923fcf81ecbdef37bcf2582a8
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-08-17 08:39:52 -0600

    Update 76-MinimumWindowSubstring.cpp

--- 76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring.cpp
@@ -1,63 +1,54 @@
-bool Valid_Window(std::unordered_map<char, int>& s_occurances, std::unordered_map<char, int>& t_occurances) {
-
-
-	for (auto& letter : t_occurances) {
-		
-		if (s_occurances[letter.first] < letter.second) {
-			return false;
-		}
-
-	}
-
-	return true;
-}
-
+#include <vector>
+class Solution {
+public:
 std::string minWindow(std::string s, std::string t) {
 
-	bool valid_flag = false;
-	int smallest_window_start = 0;
-	int smallest_window_length = s.length();
 	int back_idx = 0;
-
-	std::unordered_map<char, int> t_occurances;
-	std::unordered_map<char, int> s_occurances;
+	int unique_count = 0;
+	int result_length = INT_MAX;
+	std::pair<int, int> result_idx;
+	std::vector<int> needed_characters(58, 0);
+	std::vector<int> current_characters(58, 0);
+	
 
 	for (int i = 0; i < t.length(); i++) {
-		t_occurances[t[i]]++;
+		needed_characters[t[i]-'A']++;
+		if (needed_characters[t[i]-'A'] == 1) {
+			unique_count++;
+		}
 	}
 
 	for (int i = 0; i < s.length(); i++) {
 
-		s_occurances[s[i]]++;
+		if (needed_characters[s[i]-'A'] == 0) {
+			continue;
+		}
+		
+		current_characters[s[i]-'A']++;
 
-		if (t_occurances.find(s[i]) != t_occurances.end() && Valid_Window(s_occurances, t_occurances)) {
-
-			valid_flag = true;
-
-			while (back_idx <= i) {
-
-				if (smallest_window_length > (i - back_idx + 1)) {
-					smallest_window_length = (i - back_idx + 1);
-					smallest_window_start = back_idx;
-				}
-
-				s_occurances[s[back_idx]]--;
-
-				if (t_occurances.find(s[back_idx]) != t_occurances.end() && s_occurances[s[back_idx]] < t_occurances[s[back_idx]]) {
-					back_idx++;
-					break;
-				}
-				back_idx++;
-			}
-
-
+		if (current_characters[s[i]-'A'] == needed_characters[s[i]-'A']) {
+			unique_count--;
 		}
 
+		while (unique_count == 0) {
+
+			if (result_length > i - back_idx + 1) {
+				result_idx = { back_idx, i - back_idx + 1 };
+				result_length = i - back_idx + 1;
+			}
+
+			if (needed_characters[s[back_idx]-'A'] != 0 && current_characters[s[back_idx]-'A'] == needed_characters[s[back_idx]-'A']) {
+				unique_count++;
+			}
+
+			if (needed_characters[s[back_idx]-'A'] != 0) {
+				current_characters[s[back_idx]-'A']--;
+			}
+
+			back_idx++;
+		}
 	}
 
-	if (!valid_flag) {
-		return "";
-	}
-	return s.substr(smallest_window_start, smallest_window_length);
+	return s.substr(result_idx.first, result_idx.second);
 }
 };
