349b54f4144fc79e96c3f64272695ecb9065cfa5
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-09-01 19:04:19 -0600

    Update 2707-ExtraCharactersinaString.cpp

--- 2707-ExtraCharactersinaString.cpp
+++ 2707-ExtraCharactersinaString.cpp
@@ -2,40 +2,37 @@
 
 class Solution {
 public:
-int Solve_Max_Covered(std::vector<std::pair<int, int>>& substrings, int idx, int lowest_valid_idx, std::vector<int>& cache) {
+int Solve_Min(std::string& s, int idx, std::vector<std::string>& dictionary, std::vector<int>& cache) {
 
-	if (idx >= substrings.size()) return 0;
-	
-	if (lowest_valid_idx > substrings[idx].first) return Solve_Max_Covered(substrings, idx + 1, lowest_valid_idx, cache);
-	
-	if (cache[idx] != -1) return cache[idx];
-	
-	int skips = Solve_Max_Covered(substrings, idx + 1, lowest_valid_idx, cache);
+	if (idx >= s.length()) {
+		return 0;
+	}
 
-	int takes = substrings[idx].second - substrings[idx].first + 1 + Solve_Max_Covered(substrings, idx + 1, substrings[idx].second + 1, cache);
+	if (cache[idx] != -1) {
+		return cache[idx];
+	}
 
-	cache[idx] = (takes > skips) ? takes : skips;
-
-	return cache[idx];
-}
-
-int minExtraChar(std::string s, std::vector<std::string>& dictionary) {
-
-	std::vector<std::pair<int, int>> valid_substring_ranges;
-	std::vector<int> cache;
+	int current = 1 + Solve_Min(s, idx + 1, dictionary, cache);
+	int best = current;
 
 	for (int i = 0; i < dictionary.size(); i++) {
 
-		auto It = s.find(dictionary[i]);
-		while (It != std::string::npos) {
-			valid_substring_ranges.push_back({ It,It + dictionary[i].length() - 1 });
-			It = s.find(dictionary[i], It + 1);
+		if (s.substr(idx, dictionary[i].length()) != dictionary[i]) {
+			continue;
 		}
-	}
-	std::sort(valid_substring_ranges.begin(), valid_substring_ranges.end());
 
-	cache.resize(valid_substring_ranges.size(), -1);
+		current = Solve_Min(s, idx + dictionary[i].length(), dictionary, cache);
+		best = (best < current) ? best : current;
+	}
+
+	cache[idx] = best;
+	return best;
+}
+
+int minExtraChar(std::string s, std::vector<std::string>& dictionary) {
 	
-	return s.length() - Solve_Max_Covered(valid_substring_ranges, 0, 0, cache);
+	std::vector<int> cache(s.length(), -1);
+
+	return Solve_Min(s, 0, dictionary, cache);
 }
 };
